8228e975d678ff4975f614b3124424dd435d67bb
U-DSP\tpannek <tpannek@d-s1-21025.dsp.in.de>
2013-11-15 11:56:29 +0100

    init commit

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y apache2
-rm -rf /var/www
-ln -fs /www /var/www
+
+sudo apt-get install openjdk-7-jre-headless -y
+sudo apt-get install curl
+sudo apt-get install npm
+
+### Check http://www.elasticsearch.org/download/ for latest version of ElasticSearch and replace wget link below
+cd /opt
+ 
+# NEW WAY / EASY WAY
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb
+sudo dpkg -i elasticsearch-0.90.7.deb
+sudo service elasticsearch start
